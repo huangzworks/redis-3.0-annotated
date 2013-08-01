5f402dbc343745fb9437393a397537bb7aa06380
huangz1990 <huangz1990@gmail.com>
2013-08-02 06:50:09 +0800

    添加 anet.c 的注释

--- src/anet.c
+++ src/anet.c
@@ -47,6 +47,9 @@
 
 #include "anet.h"
 
+/*
+ * 打印错误信息
+ */
 static void anetSetError(char *err, const char *fmt, ...)
 {
     va_list ap;
@@ -57,6 +60,9 @@ static void anetSetError(char *err, const char *fmt, ...)
     va_end(ap);
 }
 
+/*
+ * 将 fd 设置为非阻塞模式（O_NONBLOCK）
+ */
 int anetNonBlock(char *err, int fd)
 {
     int flags;
@@ -77,7 +83,10 @@ int anetNonBlock(char *err, int fd)
 
 /* Set TCP keep alive option to detect dead peers. The interval option
  * is only used for Linux as we are using Linux-specific APIs to set
- * the probe send time, interval, and count. */
+ * the probe send time, interval, and count.
+ *
+ * 修改 TCP 连接的 keep alive 选项
+ */
 int anetKeepAlive(char *err, int fd, int interval)
 {
     int val = 1;
@@ -122,6 +131,9 @@ int anetKeepAlive(char *err, int fd, int interval)
     return ANET_OK;
 }
 
+/*
+ * 打开或关闭 Nagle 算法
+ */
 static int anetSetTcpNoDelay(char *err, int fd, int val)
 {
     if (setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &val, sizeof(val)) == -1)
@@ -132,17 +144,25 @@ static int anetSetTcpNoDelay(char *err, int fd, int val)
     return ANET_OK;
 }
 
+/*
+ * 禁用 Nagle 算法
+ */
 int anetEnableTcpNoDelay(char *err, int fd)
 {
     return anetSetTcpNoDelay(err, fd, 1);
 }
 
+/*
+ * 启用 Nagle 算法
+ */
 int anetDisableTcpNoDelay(char *err, int fd) 
 {
     return anetSetTcpNoDelay(err, fd, 0);
 }
 
-
+/*
+ * 设置 socket 的最大发送 buffer 字节数
+ */
 int anetSetSendBuffer(char *err, int fd, int buffsize)
 {
     if (setsockopt(fd, SOL_SOCKET, SO_SNDBUF, &buffsize, sizeof(buffsize)) == -1)
@@ -153,6 +173,9 @@ int anetSetSendBuffer(char *err, int fd, int buffsize)
     return ANET_OK;
 }
 
+/*
+ * 开启 TCP 的 keep alive 选项
+ */
 int anetTcpKeepAlive(char *err, int fd)
 {
     int yes = 1;
@@ -163,6 +186,9 @@ int anetTcpKeepAlive(char *err, int fd)
     return ANET_OK;
 }
 
+/*
+ * 解释 host 的地址，并保存到 ipbuf 中
+ */
 int anetResolve(char *err, char *host, char *ipbuf, size_t ipbuf_len)
 {
     struct addrinfo hints, *info;
@@ -199,6 +225,9 @@ static int anetSetReuseAddr(char *err, int fd) {
     return ANET_OK;
 }
 
+/*
+ * 创建并返回 socket
+ */
 static int anetCreateSocket(char *err, int domain) {
     int s;
     if ((s = socket(domain, SOCK_STREAM, 0)) == -1) {
@@ -261,11 +290,17 @@ end:
     return s;
 }
 
+/*
+ * 创建阻塞 TCP 连接
+ */
 int anetTcpConnect(char *err, char *addr, int port)
 {
     return anetTcpGenericConnect(err,addr,port,ANET_CONNECT_NONE);
 }
 
+/*
+ * 创建非阻塞 TCP 连接
+ */
 int anetTcpNonBlockConnect(char *err, char *addr, int port)
 {
     return anetTcpGenericConnect(err,addr,port,ANET_CONNECT_NONBLOCK);
@@ -297,11 +332,17 @@ int anetUnixGenericConnect(char *err, char *path, int flags)
     return s;
 }
 
+/*
+ * 创建阻塞本地连接
+ */
 int anetUnixConnect(char *err, char *path)
 {
     return anetUnixGenericConnect(err,path,ANET_CONNECT_NONE);
 }
 
+/*
+ * 创建非阻塞本地连接
+ */
 int anetUnixNonBlockConnect(char *err, char *path)
 {
     return anetUnixGenericConnect(err,path,ANET_CONNECT_NONBLOCK);
@@ -309,6 +350,9 @@ int anetUnixNonBlockConnect(char *err, char *path)
 
 /* Like read(2) but make sure 'count' is read before to return
  * (unless error or EOF condition is encountered) */
+/*
+ * 带 short count 处理的读取函数
+ */
 int anetRead(int fd, char *buf, int count)
 {
     int nread, totlen = 0;
@@ -324,6 +368,9 @@ int anetRead(int fd, char *buf, int count)
 
 /* Like write(2) but make sure 'count' is read before to return
  * (unless error is encountered) */
+/*
+ * 带 short count 处理的写入函数
+ */
 int anetWrite(int fd, char *buf, int count)
 {
     int nwritten, totlen = 0;
@@ -337,6 +384,9 @@ int anetWrite(int fd, char *buf, int count)
     return totlen;
 }
 
+/*
+ * 绑定并创建监听套接字
+ */
 static int anetListen(char *err, int s, struct sockaddr *sa, socklen_t len) {
     if (bind(s,sa,len) == -1) {
         anetSetError(err, "bind: %s", strerror(errno));
@@ -412,6 +462,9 @@ int anetTcp6Server(char *err, int port, char *bindaddr)
     return _anetTcpServer(err, port, bindaddr, AF_INET6);
 }
 
+/*
+ * 创建一个本地连接用的服务器监听套接字
+ */
 int anetUnixServer(char *err, char *path, mode_t perm)
 {
     int s;
@@ -447,6 +500,9 @@ static int anetGenericAccept(char *err, int s, struct sockaddr *sa, socklen_t *l
     return fd;
 }
 
+/*
+ * TCP 连接 accept 函数
+ */
 int anetTcpAccept(char *err, int s, char *ip, size_t ip_len, int *port) {
     int fd;
     struct sockaddr_storage sa;
@@ -466,6 +522,9 @@ int anetTcpAccept(char *err, int s, char *ip, size_t ip_len, int *port) {
     return fd;
 }
 
+/*
+ * 本地连接 accept 函数
+ */
 int anetUnixAccept(char *err, int s) {
     int fd;
     struct sockaddr_un sa;
@@ -476,6 +535,9 @@ int anetUnixAccept(char *err, int s) {
     return fd;
 }
 
+/*
+ * 获取连接客户端的 IP 和端口号
+ */
 int anetPeerToString(int fd, char *ip, size_t ip_len, int *port) {
     struct sockaddr_storage sa;
     socklen_t salen = sizeof(sa);
@@ -498,6 +560,9 @@ int anetPeerToString(int fd, char *ip, size_t ip_len, int *port) {
     return 0;
 }
 
+/*
+ * 获取服务器本机的 IP 和端口号
+ */
 int anetSockName(int fd, char *ip, size_t ip_len, int *port) {
     struct sockaddr_storage sa;
     socklen_t salen = sizeof(sa);
