49d99dabedb46db755bebefdaf5fe9c90dd2d0f0
ardxwe <ardxwe@gmail.com>
2020-03-18 15:38:57 +0800

    change-comment

--- src/redis.h
+++ src/redis.h
@@ -455,7 +455,7 @@ typedef struct redisDb {
     // 数据库键空间，保存着数据库中的所有键值对
     dict *dict;                 /* The keyspace for this DB */
 
-    // 键的过期时间，字典的键为键，字典的值为过期事件 UNIX 时间戳
+    // 键的过期时间，字典的键为键，字典的值为过期时间 UNIX 时间戳
     dict *expires;              /* Timeout of keys with a timeout set */
 
     // 正处于阻塞状态的键
